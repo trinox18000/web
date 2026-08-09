a1a4195faa95ed9b8bf83c75e1540b64e766649c
TRINOX <72662301+trinox18000@users.noreply.github.com>
2026-08-09 23:12:34 +0200

    up

--- games.js
+++ games.js
@@ -4,7 +4,7 @@
 const gamesData = [
     {
         title: "Stalker 2: Heart of Chornobyl",
-        url: "stalker2.html",
+        url: "pages jeux/stalker2.html",
         image: "assets/stalker2/images/cover.jpg",
         description: "Plongez dans la Zone d'exclusion de Tchernobyl dans ce jeu de tir et de survie.",
         tag: "Survie / FPS",
@@ -13,7 +13,7 @@ const gamesData = [
     },
     {
         title: "Super Course Auto",
-        url: "supercourse.html",
+        url: "pages jeux/supercourse.html",
         image: "images/jeu2-cover.jpg",
         description: "Un jeu de course arcade ultra rapide avec des graphismes modernes.",
         tag: "Voiture",
